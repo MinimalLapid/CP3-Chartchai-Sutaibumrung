94aeb5893dc3cb0886fc43625bfe89d653ad972d
MinimalLapid <79795775+MinimalLapid@users.noreply.github.com>
2021-03-07 16:13:45 +0700

    Add files via upload

---
+++ Ecercise4_Chartchai.py
@@ -0,0 +1,9 @@
+FD_ENG = 50.45
+GN_BUS = 73.14
+ICS    = 65
+CP     = 95.38
+print("----Your Score----")
+print("Foundation English             "," : ",FD_ENG)
+print("Generall Business              "," : ",GN_BUS)
+print("Introdution to Computer Systems"," : ",ICS)
+print("Computer Prohgramming          "," : ",CP )
